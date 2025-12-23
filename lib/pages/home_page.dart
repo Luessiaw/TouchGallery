76fac4e8893fcd2be9fc62db947172f4c6cc477a
luessiaw <luessiaw@qq.com>
2025-12-24 07:12:48 +0800

    change text to grid

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,12 +3,47 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  static const int _mockPhotoCount = 60;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Photo Manager')),
-      body: const Center(
-        child: Text('Hello, Photo Manager', style: TextStyle(fontSize: 18)),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(4),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, // 每行 3 张
+          crossAxisSpacing: 4,
+          mainAxisSpacing: 4,
+        ),
+        itemCount: _mockPhotoCount,
+        itemBuilder: (context, index) {
+          return PhotoTile(index: index);
+        },
+      ),
+    );
+  }
+}
+
+class PhotoTile extends StatelessWidget {
+  final int index;
+
+  const PhotoTile({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        debugPrint('Tapped photo $index');
+      },
+      child: Container(
+        color: Colors.grey.shade300,
+        child: Center(
+          child: Text(
+            '$index',
+            style: const TextStyle(color: Colors.black54, fontSize: 16),
+          ),
+        ),
       ),
     );
   }
