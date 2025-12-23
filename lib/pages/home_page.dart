d4a58c2541bbe06cbd60d03c86d3a74f804793ab
luessiaw <luessiaw@qq.com>
2025-12-24 07:15:08 +0800

    add scroll delete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -25,25 +25,62 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class PhotoTile extends StatelessWidget {
+class PhotoTile extends StatefulWidget {
   final int index;
 
   const PhotoTile({super.key, required this.index});
 
   @override
+  State<PhotoTile> createState() => _PhotoTileState();
+}
+
+class _PhotoTileState extends State<PhotoTile> {
+  double _dragOffsetY = 0.0;
+  static const double _deleteThreshold = -80.0;
+
+  @override
   Widget build(BuildContext context) {
+    final bool isDeleting = _dragOffsetY < _deleteThreshold;
+
     return GestureDetector(
-      onTap: () {
-        debugPrint('Tapped photo $index');
+      onVerticalDragUpdate: (details) {
+        setState(() {
+          _dragOffsetY += details.delta.dy;
+          if (_dragOffsetY > 0) _dragOffsetY = 0; // 只允许上滑
+        });
       },
-      child: Container(
-        color: Colors.grey.shade300,
-        child: Center(
-          child: Text(
-            '$index',
-            style: const TextStyle(color: Colors.black54, fontSize: 16),
+      onVerticalDragEnd: (_) {
+        if (_dragOffsetY < _deleteThreshold) {
+          debugPrint('Request delete photo ${widget.index}');
+        }
+        setState(() {
+          _dragOffsetY = 0;
+        });
+      },
+      child: Stack(
+        children: [
+          // 背景：删除提示
+          Container(
+            color: Colors.red.shade400,
+            alignment: Alignment.topCenter,
+            padding: const EdgeInsets.only(top: 8),
+            child: const Icon(Icons.delete, color: Colors.white),
           ),
-        ),
+
+          // 前景：照片本体
+          Transform.translate(
+            offset: Offset(0, _dragOffsetY),
+            child: Container(
+              color: isDeleting ? Colors.red.shade200 : Colors.grey.shade300,
+              child: Center(
+                child: Text(
+                  '${widget.index}',
+                  style: const TextStyle(fontSize: 16, color: Colors.black54),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
