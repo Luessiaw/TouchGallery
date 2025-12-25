c68a9108f9b0ad2f506aa7cceeaa41cb67475d2e
luessiaw <luessiaw@qq.com>
2025-12-25 22:46:21 +0800

    修改两处被建议替换的函数。

--- lib/pages/photo_viewer_page.dart
+++ lib/pages/photo_viewer_page.dart
@@ -18,11 +18,14 @@ class PhotoViewerPage extends StatefulWidget {
 
 class _PhotoViewerPageState extends State<PhotoViewerPage> {
   late final PageController _controller;
+  late final TransformationController _transformationController;
+  TapDownDetails? _doubleTapDetails;
 
   @override
   void initState() {
     super.initState();
     _controller = PageController(initialPage: widget.initialIndex);
+    _transformationController = TransformationController();
   }
 
   @override
@@ -34,11 +37,40 @@ class _PhotoViewerPageState extends State<PhotoViewerPage> {
         itemCount: widget.photos.length,
         itemBuilder: (context, index) {
           final asset = widget.photos[index];
-          return Center(
-            child: AssetEntityImage(
-              asset,
-              isOriginal: false,
-              fit: BoxFit.contain,
+          return GestureDetector(
+            onDoubleTapDown: (details) {
+              _doubleTapDetails = details;
+            },
+            onDoubleTap: () {
+              final position = _doubleTapDetails!.localPosition;
+
+              final currentScale = _transformationController.value
+                  .getMaxScaleOnAxis();
+
+              if (currentScale > 1.0) {
+                _transformationController.value = Matrix4.identity();
+              } else {
+                _transformationController.value = Matrix4.identity()
+                  ..translateByDouble(
+                    -position.dx * 1.5,
+                    -position.dy * 1.5,
+                    0.0,
+                    1.0,
+                  )
+                  ..scaleByDouble(3.0, 3.0, 1.0, 1.0);
+              }
+            },
+            child: InteractiveViewer(
+              transformationController: _transformationController,
+              minScale: 1.0,
+              maxScale: 4.0,
+              child: Center(
+                child: AssetEntityImage(
+                  asset,
+                  isOriginal: false,
+                  fit: BoxFit.contain,
+                ),
+              ),
             ),
           );
         },
