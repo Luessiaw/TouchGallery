f5ff4b3b1f447a2a000fc98ddfdc171b6a637594
luessiaw <luessiaw@pku.edu.cn>
2026-05-22 19:55:56 +0800

    去掉底部导航栏

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'pages/albums_page.dart';
-import 'pages/timeline_page.dart';
-import 'pages/settings_page.dart';
 import 'package:flutter/foundation.dart';
 import 'services/settings_service.dart';
 
@@ -28,43 +26,7 @@ class PhotoManagerApp extends StatelessWidget {
     return MaterialApp(
       title: 'Photo Manager',
       theme: ThemeData(useMaterial3: true),
-      home: const MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int _currentIndex = 0;
-
-  final _pages = const [AlbumsPage(), TimelinePage(), SettingsPage()];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.photo_album), label: '相册'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: '日期',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: '设置'),
-        ],
-      ),
+      home: const AlbumsPage(),
     );
   }
 }
